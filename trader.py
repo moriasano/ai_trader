ad7ff1ce71d4e9fcfea11deadb6339bf79f4888e
Mori Asano <moasano@cisco.com>
2022-02-15 12:57:15 -0500

    Fix dependacnies

---
+++ trader.py
@@ -0,0 +1,31 @@
+import datetime
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+
+
+class StockData:
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.raw_data = yf.Ticker(symbol)
+
+        self.price_history = None
+        self.get_price_data()
+
+    def get_price_data(self, granularity='15m', days_ago=30):
+        today = datetime.date.today()
+        start_date = today - datetime.timedelta(days=days_ago)
+        self.price_history = self.raw_data.history(start=start_date, end=today, interval=granularity)
+
+    def generate_graph(self, x_axis="High"):
+        self.price_history[x_axis].plot()
+        plt.show()
+
+
+if __name__ == '__main__':
+    s = StockData("CSCO")
+
+    s.generate_graph()
